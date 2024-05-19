b1d98812f50529d3886ac812b78bab0fe68d8a70
fleurfanli <victoriousmaximoff@gmail.com>
2024-05-19 12:38:12 +0800

    modify init

--- src/lantern/model/basis/__init__.py
+++ src/lantern/model/basis/__init__.py
@@ -1,3 +1,10 @@
-from lantern.model.basis.basis import Basis
-from lantern.model.basis.variational import VariationalBasis
-from lantern.model.basis.language import LanguageBasis
+from .basis import Basis
+from .variational import VariationalBasis
+from .language import LanguageBasis
+
+__all__ = ['Basis', 'VariationalBasis', 'LanguageBasis']
+
+
+# from lantern.model.basis.basis import Basis
+# from lantern.model.basis.variational import VariationalBasis
+# from lantern.model.basis.language import LanguageBasis
