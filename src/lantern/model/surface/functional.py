a1931c1e6fa6f7b7c0dba26cd076bee23c10d3b6
fleurfanli <victoriousmaximoff@gmail.com>
2024-10-06 21:53:25 -0400

    update for binary classification

--- src/lantern/model/surface/functional.py
+++ src/lantern/model/surface/functional.py
@@ -5,9 +5,10 @@ from gpytorch.variational import VariationalStrategy
 from gpytorch.distributions import MultivariateNormal
 from gpytorch.variational import IndependentMultitaskVariationalStrategy
 from gpytorch.means import ConstantMean, Mean
-from gpytorch.kernels import PolynomialKernel, ScaleKernel, RQKernel, Kernel
-import torch
+from gpytorch.kernels import PolynomialKernel, ScaleKernel, Kernel
 
+import torch
+from torch.nn.functional import sigmoid 
 
 from lantern.model.surface import Surface
 
@@ -87,7 +88,9 @@ class Functional(ApproximateGP, Surface):
         """
         mean_x = self.mean(z)
         covar_x = self.kernel(z)
-        return MultivariateNormal(mean_x, covar_x)
+        
+        # use sigmoid to return probabilities for binary classification
+        return MultivariateNormal(sigmoid(mean_x), covar_x)
 
     @classmethod
     def fromDataset(cls, Z, ds, *args, **kwargs):
